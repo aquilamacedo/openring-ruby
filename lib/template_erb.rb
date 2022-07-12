7b6e8cf045a95a89186f3c39b443eff254029128
Aquila Macedo <aquilamacedo@riseup.net>
2022-07-12 11:57:40 -0300

    lib: template_erb: implementing rss class.
    This commit finalizes the implementation of the RSS class's methods.
    
    Signed-off-by: Aquila Macedo <aquilamacedo@riseup.net>

--- lib/template_erb.rb
+++ lib/template_erb.rb
@@ -2,7 +2,7 @@ require_relative 'Feed.rb'
 
 TEMPLATE_HTML = 'templates/in.html'
 
-ATOM_CONTENTS = ['https://chaws.me/feed.xml', 'https://siqueira.tech/feed.xml', 'https://siqueira.tech/feed.xml']
+ATOM_CONTENTS = ['https://chaws.me/feed.xml', 'https://siqueira.tech/feed.xml', 'https://drewdevault.com/blog/index.xml']
 
 process_title = []
 process_link = []
@@ -15,13 +15,24 @@ for value in ATOM_CONTENTS do
   rss = RSS::Parser.parse(value)
   
   a = Atom.new
+  b = Rss.new
 
-  process_title.push(a.process_title(rss))
-  process_link.push(a.process_link(rss))
-  process_post_content.push(a.process_post_content(rss))
-  process_post_date.push(a.process_post_date(rss))
-  process_post_title.push(a.process_post_title(rss))
-  process_post_link.push(a.process_post_link(rss))
+  case rss.feed_type
+    when 'atom'
+      process_title.push(a.process_title(rss))
+      process_link.push(a.process_link(rss))
+      process_post_content.push(a.process_post_content(rss))
+      process_post_date.push(a.process_post_date(rss))
+      process_post_title.push(a.process_post_title(rss))
+      process_post_link.push(a.process_post_link(rss))
+    when 'rss'
+      process_title.push(b.process_title(rss))
+      process_link.push(b.process_link(rss))
+      process_post_content.push(b.process_post_content(rss))
+      process_post_date.push(b.process_post_date(rss))
+      process_post_title.push(b.process_post_title(rss))
+      process_post_link.push(b.process_post_link(rss))
+  end
 end
 
 
